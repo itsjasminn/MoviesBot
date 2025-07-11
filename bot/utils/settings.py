3941d99917de774d3087a5e980995504f962f83b
almuvvahhida <muvvahhida@gmail.com>
2025-07-11 11:53:26 +0500

    updated

--- bot/utils/settings.py
+++ bot/utils/settings.py
@@ -1,5 +1,6 @@
 from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeChat
+
 from db.base import db as database
 
 ADMINS_IDS = 7556384250,
@@ -11,7 +12,7 @@ async def on_startup(bot: Bot):
     await database.create_all()
     await bot.set_my_commands(commands)
     for admin in ADMINS_IDS:
-        await bot.set_my_commands([BotCommand(command="admin", description="admin panel"),],
+        await bot.set_my_commands([BotCommand(command="admin", description="admin panel"), ],
                                   BotCommandScopeChat(chat_id=admin))
 
 
