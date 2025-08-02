0870e0cfb8fbd74270315b647fdba0b49745b9a8
itsjasminn <muvvahhida@gmail.com>
2025-08-02 19:59:49 +0500

    updated

--- bot/handler/comedy_movies_handler.py
+++ bot/handler/comedy_movies_handler.py
@@ -14,8 +14,9 @@ from bot.states import SectorState
 @dp.message(SectorState.yes_hand_comedy_dump, F.text == __("⬅️ Back"))
 @dp.message(SectorState.home_alone, F.text == __("⬅️ Back"))
 async def comedy_handler(message: Message, state: FSMContext):
-    texts = [_("🎬 Home Alone (1990)"), _("🎬 Dumb and Dumber (1994)"), _("🎬 The Mask (1994)"),
-             _("🎬 Mrs. Doubtfire (1993)"), _("⬅️ Back")]
+    texts = [_("🎬 Home Alone (1990)"), _("⬅️ Back")]
+    # _("🎬 Dumb and Dumber (1994)"), _("🎬 The Mask (1994)"),
+    # _("🎬 Mrs. Doubtfire (1993)"),
     markup = build_reply_button(texts, (2, 2, 1))
     await state.set_state(SectorState.comedy_section)
     await message.answer(_("Choose the movie."), reply_markup=markup)
@@ -55,120 +56,119 @@ async def yes_handler_comedy(message: Message, state: FSMContext):
     await state.set_state(SectorState.yes_hand_comedy)
     await message.answer_video(video_id, caption=_("🎬 Home Alone (1990)"))
 
-
 ##################################################################################################################
-
-@dp.message(SectorState.comedy_section, F.text == __("🎬 Dumb and Dumber (1994)"))
-async def dump(message: Message, state: FSMContext):
-    text = (_(
-        "🎬 <b>Dumb and Dumber (1994)</b>\n"
-        "⭐ <b>IMDb Rating</b>: 7.3/10\n"
-        "🎭 <b>Genre</b>: Comedy\n"
-        "🎥 <b>Director</b>: Peter Farrelly\n"
-        "👤 <b>Starring</b>: Jim Carrey, Jeff Daniels, Lauren Holly\n"
-        "🏆 <b>Awards</b>: Nominated for 4 MTV Movie Awards 🏅\n"
-        "📖 <b>Plot Summary</b>: Lloyd Christmas and Harry Dunne, two well-meaning but extremely dim-witted "
-        "friends, embark on a hilarious cross-country road trip to return a briefcase full of money to its "
-        "rightful owner. Along the way, their clueless antics and absurd misadventures lead to non-stop "
-        "laughs in this classic comedy. 🤪🚗\n\n"
-        "Do you want to watch it?"
-    ))
-
-    rkb = ReplyKeyboardBuilder()
-    rkb.add(KeyboardButton(text=_("Yes")),
-            KeyboardButton(text=_("⬅️ Back")))
-    rkb.adjust(2, 1)
-    rkb = rkb.as_markup(resize_keyboard=True)
-    await state.set_state(SectorState.dump)
-    await message.answer(text, reply_markup=rkb, parse_mode='HTML')
-
-
-@dp.message(SectorState.dump, F.text == __("Yes"))
-async def yes_handler_dump(message: Message, state: FSMContext):
-    video_id = ""
-    rkb = ReplyKeyboardBuilder()
-    rkb.add(KeyboardButton(text=_("⬅️ Back")))
-    rkb.adjust(1)
-    rkb = rkb.as_markup(resize_keyboard=True)
-    await state.set_state(SectorState.yes_hand_comedy_dump)
-    await message.answer_video(video_id, caption=_("🎬 Dumb and Dumber (1994)"))
-
-
-##################################################################################################################
-
-@dp.message(SectorState.comedy_section, F.text == __("🎬 The Mask (1994)"))
-async def mask(message: Message, state: FSMContext):
-    text = (_(
-        "🎬 <b>The Mask (1994)</b>\n"
-        "⭐ <b>IMDb Rating</b>: 6.9/10\n"
-        "🎭 <b>Genre</b>: Comedy, Fantasy\n"
-        "🎥 <b>Director</b>: Chuck Russell\n"
-        "👤 <b>Starring</b>: Jim Carrey, Cameron Diaz, Peter Riegert\n"
-        "🏆 <b>Awards</b>: Nominated for 1 Academy Award 🏅\n"
-        "📖 <b>Plot Summary</b>: Stanley Ipkiss, a shy and unlucky bank clerk, stumbles upon a mysterious ancient "
-        "mask that transforms him into a zany, green-faced trickster with extraordinary powers. As he embraces his "
-        "new alter ego, chaos and hilarity ensue, making him the target of both the police and the city's crime boss. "
-        "A wildly entertaining comedy filled with action and over-the-top antics!\n\n"
-        "Do you want to watch it?"
-
-    ))
-
-    rkb = ReplyKeyboardBuilder()
-    rkb.add(KeyboardButton(text=_("Yes")),
-            KeyboardButton(text=_("⬅️ Back")))
-    rkb.adjust(2, 1)
-
-    rkb = rkb.as_markup(resize_keyboard=True)
-    await state.set_state(SectorState.mask)
-    await message.answer(text, reply_markup=rkb, parse_mode='HTML')
-
-
-@dp.message(SectorState.mask, F.text == __("Yes"))
-async def yes_handler_dump(message: Message, state: FSMContext):
-    video_id = ""
-    rkb = ReplyKeyboardBuilder()
-    rkb.add(KeyboardButton(text=_("⬅️ Back")))
-    rkb.adjust(1)
-    rkb = rkb.as_markup(resize_keyboard=True)
-    await state.set_state(SectorState.yes_hand_comedy_mask)
-    await message.answer_video(video_id, caption=_("🎬 The Mask (1994)"))
-
-
-##################################################################################################################
-
-@dp.message(SectorState.comedy_section, F.text == __("🎬 Mrs. Doubtfire (1993)"))
-async def doubtfire(message: Message, state: FSMContext):
-    text = (_(
-        "🎬 <b>The Mask (1994)</b>\n"
-        "⭐ <b>IMDb Rating</b>: 6.9/10\n"
-        "🎭 <b>Genre</b>: Comedy, Fantasy\n"
-        "🎥 <b>Director</b>: Chuck Russell\n"
-        "👤 <b>Starring</b>: Jim Carrey, Cameron Diaz, Peter Riegert\n"
-        "🏆 <b>Awards</b>: Nominated for 1 Academy Award 🏅\n"
-        "📖 <b>Plot Summary</b>: Stanley Ipkiss, a shy and unlucky bank clerk, stumbles upon a mysterious ancient "
-        "mask that transforms him into a zany, green-faced trickster with extraordinary powers. As he embraces his "
-        "new alter ego, chaos and hilarity ensue, making him the target of both the police and the city's crime boss. "
-        "A wildly entertaining comedy filled with action and over-the-top antics!\n\n"
-        "Do you want to watch it?"
-
-    ))
-
-    rkb = ReplyKeyboardBuilder()
-    rkb.add(KeyboardButton(text=_("Yes")),
-            KeyboardButton(text=_("⬅️ Back")))
-    rkb.adjust(2, 1)
-
-    rkb = rkb.as_markup(resize_keyboard=True)
-    await state.set_state(SectorState.doubtfire)
-    await message.answer(text, reply_markup=rkb, parse_mode='HTML')
-
-
-@dp.message(SectorState.doubtfire, F.text == __("Yes"))
-async def yes_handler_doubtfire(message: Message, state: FSMContext):
-    video_id = ""
-    rkb = ReplyKeyboardBuilder()
-    rkb.add(KeyboardButton(text=_("⬅️ Back")))
-    rkb.adjust(1)
-    rkb = rkb.as_markup(resize_keyboard=True)
-    await state.set_state(SectorState.yes_hand_comedy_doubtfire)
-    await message.answer_video(video_id, caption=_("🎬 Mrs. Doubtfire (1993)"))
+#
+# @dp.message(SectorState.comedy_section, F.text == __("🎬 Dumb and Dumber (1994)"))
+# async def dump(message: Message, state: FSMContext):
+#     text = (_(
+#         "🎬 <b>Dumb and Dumber (1994)</b>\n"
+#         "⭐ <b>IMDb Rating</b>: 7.3/10\n"
+#         "🎭 <b>Genre</b>: Comedy\n"
+#         "🎥 <b>Director</b>: Peter Farrelly\n"
+#         "👤 <b>Starring</b>: Jim Carrey, Jeff Daniels, Lauren Holly\n"
+#         "🏆 <b>Awards</b>: Nominated for 4 MTV Movie Awards 🏅\n"
+#         "📖 <b>Plot Summary</b>: Lloyd Christmas and Harry Dunne, two well-meaning but extremely dim-witted "
+#         "friends, embark on a hilarious cross-country road trip to return a briefcase full of money to its "
+#         "rightful owner. Along the way, their clueless antics and absurd misadventures lead to non-stop "
+#         "laughs in this classic comedy. 🤪🚗\n\n"
+#         "Do you want to watch it?"
+#     ))
+#
+#     rkb = ReplyKeyboardBuilder()
+#     rkb.add(KeyboardButton(text=_("Yes")),
+#             KeyboardButton(text=_("⬅️ Back")))
+#     rkb.adjust(2, 1)
+#     rkb = rkb.as_markup(resize_keyboard=True)
+#     await state.set_state(SectorState.dump)
+#     await message.answer(text, reply_markup=rkb, parse_mode='HTML')
+#
+#
+# @dp.message(SectorState.dump, F.text == __("Yes"))
+# async def yes_handler_dump(message: Message, state: FSMContext):
+#     video_id = ""
+#     rkb = ReplyKeyboardBuilder()
+#     rkb.add(KeyboardButton(text=_("⬅️ Back")))
+#     rkb.adjust(1)
+#     rkb = rkb.as_markup(resize_keyboard=True)
+#     await state.set_state(SectorState.yes_hand_comedy_dump)
+#     await message.answer_video(video_id, caption=_("🎬 Dumb and Dumber (1994)"))
+#
+#
+# ##################################################################################################################
+#
+# @dp.message(SectorState.comedy_section, F.text == __("🎬 The Mask (1994)"))
+# async def mask(message: Message, state: FSMContext):
+#     text = (_(
+#         "🎬 <b>The Mask (1994)</b>\n"
+#         "⭐ <b>IMDb Rating</b>: 6.9/10\n"
+#         "🎭 <b>Genre</b>: Comedy, Fantasy\n"
+#         "🎥 <b>Director</b>: Chuck Russell\n"
+#         "👤 <b>Starring</b>: Jim Carrey, Cameron Diaz, Peter Riegert\n"
+#         "🏆 <b>Awards</b>: Nominated for 1 Academy Award 🏅\n"
+#         "📖 <b>Plot Summary</b>: Stanley Ipkiss, a shy and unlucky bank clerk, stumbles upon a mysterious ancient "
+#         "mask that transforms him into a zany, green-faced trickster with extraordinary powers. As he embraces his "
+#         "new alter ego, chaos and hilarity ensue, making him the target of both the police and the city's crime boss. "
+#         "A wildly entertaining comedy filled with action and over-the-top antics!\n\n"
+#         "Do you want to watch it?"
+#
+#     ))
+#
+#     rkb = ReplyKeyboardBuilder()
+#     rkb.add(KeyboardButton(text=_("Yes")),
+#             KeyboardButton(text=_("⬅️ Back")))
+#     rkb.adjust(2, 1)
+#
+#     rkb = rkb.as_markup(resize_keyboard=True)
+#     await state.set_state(SectorState.mask)
+#     await message.answer(text, reply_markup=rkb, parse_mode='HTML')
+#
+#
+# @dp.message(SectorState.mask, F.text == __("Yes"))
+# async def yes_handler_dump(message: Message, state: FSMContext):
+#     video_id = ""
+#     rkb = ReplyKeyboardBuilder()
+#     rkb.add(KeyboardButton(text=_("⬅️ Back")))
+#     rkb.adjust(1)
+#     rkb = rkb.as_markup(resize_keyboard=True)
+#     await state.set_state(SectorState.yes_hand_comedy_mask)
+#     await message.answer_video(video_id, caption=_("🎬 The Mask (1994)"))
+#
+#
+# ##################################################################################################################
+#
+# @dp.message(SectorState.comedy_section, F.text == __("🎬 Mrs. Doubtfire (1993)"))
+# async def doubtfire(message: Message, state: FSMContext):
+#     text = (_(
+#         "🎬 <b>The Mask (1994)</b>\n"
+#         "⭐ <b>IMDb Rating</b>: 6.9/10\n"
+#         "🎭 <b>Genre</b>: Comedy, Fantasy\n"
+#         "🎥 <b>Director</b>: Chuck Russell\n"
+#         "👤 <b>Starring</b>: Jim Carrey, Cameron Diaz, Peter Riegert\n"
+#         "🏆 <b>Awards</b>: Nominated for 1 Academy Award 🏅\n"
+#         "📖 <b>Plot Summary</b>: Stanley Ipkiss, a shy and unlucky bank clerk, stumbles upon a mysterious ancient "
+#         "mask that transforms him into a zany, green-faced trickster with extraordinary powers. As he embraces his "
+#         "new alter ego, chaos and hilarity ensue, making him the target of both the police and the city's crime boss. "
+#         "A wildly entertaining comedy filled with action and over-the-top antics!\n\n"
+#         "Do you want to watch it?"
+#
+#     ))
+#
+#     rkb = ReplyKeyboardBuilder()
+#     rkb.add(KeyboardButton(text=_("Yes")),
+#             KeyboardButton(text=_("⬅️ Back")))
+#     rkb.adjust(2, 1)
+#
+#     rkb = rkb.as_markup(resize_keyboard=True)
+#     await state.set_state(SectorState.doubtfire)
+#     await message.answer(text, reply_markup=rkb, parse_mode='HTML')
+#
+#
+# @dp.message(SectorState.doubtfire, F.text == __("Yes"))
+# async def yes_handler_doubtfire(message: Message, state: FSMContext):
+#     video_id = ""
+#     rkb = ReplyKeyboardBuilder()
+#     rkb.add(KeyboardButton(text=_("⬅️ Back")))
+#     rkb.adjust(1)
+#     rkb = rkb.as_markup(resize_keyboard=True)
+#     await state.set_state(SectorState.yes_hand_comedy_doubtfire)
+#     await message.answer_video(video_id, caption=_("🎬 Mrs. Doubtfire (1993)"))
